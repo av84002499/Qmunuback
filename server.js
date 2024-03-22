725294eb01a160e1b4e87e0998a15fa0badb5826
av84002499 <109796002+av84002499@users.noreply.github.com>
2024-03-22 23:01:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ dotenv.config();
 server.use((req, res, next) => {
    res.header(
     'Access-Control-Allow-Origin',
-     'http://localhost:3000'
+     'http://localhost:3000',
+      'https://new-sage-nine.vercel.app'
 
   );
   res.header('Access-Control-Allow-Headers', '*');
