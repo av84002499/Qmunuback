4cb566bf0fe4d4fe0c40ec1c3fe720a816ef0df3
av7388605503 <av84002499@gmail.com>
2024-04-08 23:02:55 +0530

    update

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -35,6 +35,46 @@ export default class UserController {
     }
   }
 
+  async signUp(req, res, next) {
+    const {
+      name,
+      email,
+      password,
+      type,
+    } = req.body;
+    try {
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new UserModel(
+        name,
+        email,
+        hashedPassword,
+        type
+      );
+      await this.userRepository.signUp(user);
+  
+      // Send confirmation email
+      await this.sendConfirmationEmail(email);
+  
+      res.status(201).send(user);
+    } catch (err) {
+      next(err);
+    }
+  }
+  
+  async sendConfirmationEmail(email) {
+    // Send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // Sender address
+      to: email, // Receiver address
+      subject: 'Welcome to Our Website', // Subject line
+      text: 'Thank you for registering with us!', // Plain text body
+      html: '<b>Thank you for registering with us!</b>', // HTML body
+    });
+  
+    console.log('Confirmation email sent to:', email);
+    console.log('Message sent: %s', info.messageId);
+  }
+  
   async signIn(req, res, next) {
     try {
       const { email, password } = req.body;
@@ -51,7 +91,7 @@ export default class UserController {
             userID: user._id,
             email: user.email,
           },
-          'YourSecretKey', // Use your own secret key for JWT
+          'AIb6d35fvJM4O9pXqXQNla2jBCH9kuLz',
           {
             expiresIn: '1h',
           }
@@ -72,77 +112,5 @@ export default class UserController {
       console.log(err);
       return res.status(500).send("Something went wrong");
     }
-  }
-
-  async signUp(req, res, next) {
-    const {
-      name,
-      email,
-      password,
-      type,
-    } = req.body;
-    try {
-      const hashedPassword = await bcrypt.hash(password, 12);
-      const user = new UserModel(
-        name,
-        email,
-        hashedPassword,
-        type
-      );
-      await this.userRepository.signUp(user);
-
-      // Send confirmation email with link
-      await this.sendConfirmationEmail(email, user._id);
-
-      res.status(201).send({ message: 'Signup successful. Please check your email for confirmation.' });
-    } catch (err) {
-      next(err);
-    }
-  }
-
-  async  verification (req, res, next) {
-    const token = req.params.token;
-    try {
-      // Find the user by the verification token
-      const user = await this.userRepository.findByVerificationToken(token);
-      if (!user) {
-        return res.status(400).send({ message: 'Invalid or expired token.' });
-      }
-      // Update user's account status to verified
-      user.verified = true;
-      await this.userRepository.updateUser(user);
-      // Optionally: Redirect the user to a page confirming successful verification
-      res.redirect('/verified');
-    } catch (err) {
-      next(err);
-    }
-  };
-
-  async signUp(req, res, next) {
-    const { name, email, password, type } = req.body;
-    try {
-      const hashedPassword = await bcrypt.hash(password, 12);
-      const verificationToken = generateVerificationToken(); // Implement this function
-      const user = new UserModel(name, email, hashedPassword, type, verificationToken);
-      await this.userRepository.signUp(user);
-  
-      // Send confirmation email with verification link
-      const verificationLink = `${req.protocol}://${req.get('host')}/verify/${verificationToken}`;
-      await this.sendConfirmationEmail(email, verificationLink);
-  
-      res.status(201).send({ message: 'Signup successful. Please check your email for confirmation.' });
-    } catch (err) {
-      next(err);
-    }
-  }
-  
-  async sendConfirmationEmail(email, verificationLink) {
-    const info = await transporter.sendMail({
-      from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>',
-      to: email,
-      subject: 'Account Verification',
-      html: `<p>Please click <a href="${verificationLink}">here</a> to verify your account.</p>`,
-    });
-    console.log("Message sent: %s", info.messageId);
   }
 }
