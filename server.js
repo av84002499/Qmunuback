07f04a427724baffc1f82c99eedf752c48c842b3
av7388605503 <av84002499@gmail.com>
2024-05-05 08:16:05 +0530

    update

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ dotenv.config();
 server.use((req, res, next) => {
    res.header(
     'Access-Control-Allow-Origin',
-      'http://localhost:3000'
+      'https://www.quickcatalog.in'
   );
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', '*');
